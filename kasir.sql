b0caa2f27b1b6599c4d8b618a088fe4ba2f15656
Mohamad Rizal Ramli <22782888+zalramli@users.noreply.github.com>
2019-01-16 13:18:18 +0700

    --
    --

---
+++ kasir.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 16 Jan 2019 pada 07.17
+-- Versi server: 10.1.36-MariaDB
+-- Versi PHP: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `akses`
+--
+
+CREATE TABLE `akses` (
+  `id_akses` char(3) NOT NULL,
+  `nm_akses` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` varchar(30) NOT NULL,
+  `id_tipe_stok` char(3) NOT NULL,
+  `nm_barang` varchar(50) NOT NULL,
+  `jml_stok` smallint(5) NOT NULL,
+  `hrg_jual` int(10) NOT NULL,
+  `hrg_beli` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_pemasokan`
+--
+
+CREATE TABLE `detail_pemasokan` (
+  `id_detail_pemasokan` int(11) NOT NULL,
+  `id_pemasokan` char(8) NOT NULL,
+  `id_barang` varchar(30) NOT NULL,
+  `qty` smallint(5) NOT NULL,
+  `hrg_beli` int(10) NOT NULL,
+  `total_hrg` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_detail_transaksi` int(11) NOT NULL,
+  `id_transaksi` char(10) NOT NULL,
+  `id_barang` varchar(30) NOT NULL,
+  `qty` smallint(5) NOT NULL,
+  `total_hrg` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `distributor`
+--
+
+CREATE TABLE `distributor` (
+  `id_distributor` char(4) NOT NULL,
+  `nm_distributor` varchar(50) NOT NULL,
+  `no_hp` char(15) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemasokan`
+--
+
+CREATE TABLE `pemasokan` (
+  `id_pemasokan` char(8) NOT NULL,
+  `id_distributor` char(4) NOT NULL,
+  `id_user` char(3) NOT NULL,
+  `total_pasok` int(10) NOT NULL,
+  `bayar` int(10) NOT NULL,
+  `kembalian` int(10) NOT NULL,
+  `tgl_pemasokan` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tipe_stok`
+--
+
+CREATE TABLE `tipe_stok` (
+  `id_tipe_stok` char(3) NOT NULL,
+  `nm_tipe_stok` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` char(10) NOT NULL,
+  `id_user` char(3) NOT NULL,
+  `total_hrg` int(10) NOT NULL,
+  `bayar` int(10) NOT NULL,
+  `kembalian` int(10) NOT NULL,
+  `tgl_transaksi` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` char(3) NOT NULL,
+  `id_akses` char(3) NOT NULL,
+  `nm_user` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `akses`
+--
+ALTER TABLE `akses`
+  ADD PRIMARY KEY (`id_akses`);
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indeks untuk tabel `detail_pemasokan`
+--
+ALTER TABLE `detail_pemasokan`
+  ADD PRIMARY KEY (`id_detail_pemasokan`);
+
+--
+-- Indeks untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id_detail_transaksi`);
+
+--
+-- Indeks untuk tabel `distributor`
+--
+ALTER TABLE `distributor`
+  ADD PRIMARY KEY (`id_distributor`);
+
+--
+-- Indeks untuk tabel `pemasokan`
+--
+ALTER TABLE `pemasokan`
+  ADD PRIMARY KEY (`id_pemasokan`);
+
+--
+-- Indeks untuk tabel `tipe_stok`
+--
+ALTER TABLE `tipe_stok`
+  ADD PRIMARY KEY (`id_tipe_stok`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_pemasokan`
+--
+ALTER TABLE `detail_pemasokan`
+  MODIFY `id_detail_pemasokan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id_detail_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
