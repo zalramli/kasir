8a4d57a38e49ec98dc18d0e284c370d4ae0591bd
palindungan <rizkika.palindungan1@gmail.com>
2019-01-23 10:47:02 +0700

    adsasd
    kenek

--- src/Transaksi/Transaksi.java
+++ src/Transaksi/Transaksi.java
@@ -127,15 +127,15 @@ public class Transaksi extends javax.swing.JInternalFrame {
     }
 
     private void hidden() {
-//        txt_nama.setVisible(false);
-//        txt_hrg_eceran.setVisible(false);
-//        txt_hrg_grosir.setVisible(false);
-//        txt_isi_pack.setVisible(false);
-//        txt_jml_stok.setVisible(false);
-//        txt_kode.setVisible(false);
-//        txt_pilihan.setVisible(false);
-//        txt_baris.setVisible(false);
-//        txt_qty.setVisible(false);
+        txt_nama.setVisible(false);
+        txt_hrg_eceran.setVisible(false);
+        txt_hrg_grosir.setVisible(false);
+        txt_isi_pack.setVisible(false);
+        txt_jml_stok.setVisible(false);
+        txt_kode.setVisible(false);
+        txt_pilihan.setVisible(false);
+        txt_baris.setVisible(false);
+        txt_qty.setVisible(false);
     }
 
     private void inisialisasi_tabel() {
@@ -683,7 +683,10 @@ public class Transaksi extends javax.swing.JInternalFrame {
 
     private void daftar_produkKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_daftar_produkKeyReleased
         // TODO add your handling code here:
-        
+        int row = this.daftar_produk.getSelectedRow();
+        this.txt_baris.setText(String.valueOf(row));
+
+        updateHarga();
     }//GEN-LAST:event_daftar_produkKeyReleased
 
 
