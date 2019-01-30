0fb606289a0d72cd19d524429c60055bb34d3279
Mohamad Rizal Ramli <22782888+zalramli@users.noreply.github.com>
2019-01-30 18:49:36 +0700

    --
    --

--- src/Dashboard/DashboardGudang.java
+++ src/Dashboard/DashboardGudang.java
@@ -673,31 +673,11 @@ public class DashboardGudang extends javax.swing.JFrame {
 
     private void btn_hapusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_hapusActionPerformed
         // TODO add your handling code here:
-        if (txt_bayar.getText().length() > 0) {
-            list_produk.removeRow(daftar_produk.getSelectedRow());
-            btn_hapus.setEnabled(false);
-            getsum();
-            String total = txt_total.getText();
-            DecimalFormat kursIndonesia = (DecimalFormat) DecimalFormat.getCurrencyInstance();
-            DecimalFormatSymbols formatRp = new DecimalFormatSymbols();
-
-            formatRp.setCurrencySymbol("");
-            formatRp.setMonetaryDecimalSeparator(' ');
-            formatRp.setGroupingSeparator('.');
-            kursIndonesia.setDecimalFormatSymbols(formatRp);
-            try {
-                Number number = kursIndonesia.parse(total);
-                double nilai = number.doubleValue();
-                int bayar = Integer.parseInt(txt_bayar.getText());
-                double kembalian = bayar - nilai;
-                String kembalians = String.format("%,.0f", kembalian).replaceAll(",", ".");
-                txt_kembalian.setText(kembalians);
-            } catch (ParseException ex) {
-                System.out.println("Kesalahan Parsing");
-            }
-        } else {
-            JOptionPane.showMessageDialog(null, "Isikan Bayar !", "Kesalahan", JOptionPane.ERROR_MESSAGE);
-        }
+        list_produk.removeRow(daftar_produk.getSelectedRow());
+        btn_hapus.setEnabled(false);
+        getsum();
+        txt_bayar.setText("");
+        txt_kembalian.setText("0");
     }//GEN-LAST:event_btn_hapusActionPerformed
 
     private void btn_batalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_batalActionPerformed
