49ac143b864dfed208d6d3a7402afcaa410961fc
Mohamad Rizal Ramli <22782888+zalramli@users.noreply.github.com>
2019-01-31 15:09:13 +0700

    --
    --

--- src/Dashboard/DashboardKasir.java
+++ src/Dashboard/DashboardKasir.java
@@ -11,9 +11,18 @@ import Master.Distributor;
 import Transaksi.CariBarang111;
 import Transaksi.Transaksi;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
 import java.awt.HeadlessException;
 import java.awt.event.KeyEvent;
+import java.awt.print.PageFormat;
+import java.awt.print.Paper;
+import java.awt.print.Printable;
+import static java.awt.print.Printable.NO_SUCH_PAGE;
+import static java.awt.print.Printable.PAGE_EXISTS;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.sql.Connection;
 import javax.swing.JFrame;
 import javax.swing.table.DefaultTableModel;
@@ -532,11 +541,146 @@ public class DashboardKasir extends javax.swing.JFrame {
         updateHarga();
     }//GEN-LAST:event_daftar_produkKeyReleased
 
+    public PageFormat getPageFormat(PrinterJob pj)
+    {
+
+        PageFormat pf = pj.defaultPage();
+        Paper paper = pf.getPaper();    
+
+        double middleHeight =8.0;  
+        double headerHeight = 2.0;                  
+        double footerHeight = 2.0;                  
+        double width = convert_CM_To_PPI(8);      //printer know only point per inch.default value is 72ppi
+        double height = convert_CM_To_PPI(headerHeight+middleHeight+footerHeight); 
+        paper.setSize(width, height);
+        paper.setImageableArea(                    
+            0,
+            10,
+            width,            
+            height - convert_CM_To_PPI(1)
+        );   //define boarder size    after that print area width is about 180 points
+
+        pf.setOrientation(PageFormat.PORTRAIT);           //select orientation portrait or landscape but for this time portrait
+        pf.setPaper(paper);    
+
+        return pf;
+    }
+    
+    protected static double convert_CM_To_PPI(double cm) 
+    {            
+	return toPPI(cm * 0.393600787);            
+    }
+ 
+    protected static double toPPI(double inch) 
+    {            
+        return inch * 72d;            
+    }
+    
+    public class BillPrintable implements Printable 
+    {
+    
+  
+        public int print(Graphics graphics, PageFormat pageFormat,int pageIndex) 
+        throws PrinterException 
+        {    
+      
+            
+            int result = NO_SUCH_PAGE;    
+              if (pageIndex == 0) 
+                {                    
+
+                  Graphics2D g2d = (Graphics2D) graphics;
+                  double width = pageFormat.getImageableWidth();                    
+                  g2d.translate((int) pageFormat.getImageableX(),(int) pageFormat.getImageableY()); 
+
+                  
+              try{
+                  /*Draw Header*/
+                  int y=20;
+                  int yShift = 10;
+                  int headerRectHeight=15;
+                  int headerRectHeighta=40;
+                  String id_transaksi = txt_id_transaksi.getText();
+                  String nama_kasir = nama.getText();
+                  Date ys = new Date();
+                  SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy");
+                  String tggl = s.format(ys);
+                  SimpleDateFormat x = new SimpleDateFormat("HH:mm:ss");
+                  String waktu = x.format(ys);
+
+                  g2d.setFont(new Font("Monospaced",Font.PLAIN,7));
+                  g2d.drawString("        SUMBER REJEKI        ",12,y);y+=yShift;
+                  g2d.setFont(new Font("Monospaced",Font.PLAIN,6));
+                  g2d.drawString("      Jl. Raya Tongas No.189        ",12,y);y+=yShift;
+                  g2d.drawString("        Telp 081335890328          ",12,y);y+=yShift;
+                  g2d.drawString("                                     ",10,y);y+=yShift;
+                  g2d.drawString("ID  : "+id_transaksi+"    Kasir : "+nama_kasir+"   ",10,y);y+=yShift;
+                  g2d.drawString("Tgl : "+tggl+" "+waktu+"         ",10,y);y+=yShift;
+                  g2d.drawString("-------------------------------------",10,y);y+=yShift;
+                  g2d.drawString("                                     ",10,y);y+=yShift;
+                  int t = daftar_produk.getRowCount();
+                  for(int i=0; i < t ; i++)
+                  {
+                  String brg= daftar_produk.getValueAt(i,1).toString();
+                  String jml= daftar_produk.getValueAt(i,3).toString();
+                  String satuan= daftar_produk.getValueAt(i,2).toString();
+                  int hrg = Integer.parseInt(daftar_produk.getValueAt(i, 4).toString());
+                  double angka = (double) hrg;
+                  String harga = String.format("%,.0f", angka).replaceAll(",", ".");
+                  String total= daftar_produk.getValueAt(i,5).toString();
+                  int tot = Integer.parseInt(daftar_produk.getValueAt(i, 5).toString());
+                  double angka2 = (double) tot;
+                  String totals = String.format("%,.0f", angka2).replaceAll(",", ".");
+
+                  g2d.drawString(" "+brg+"                            ",10,y);y+=yShift;
+                  g2d.drawString("      "+jml+"",10,y);
+                  g2d.drawString("           "+satuan+"",10,y);
+                  g2d.drawString("                  "+harga+"",10,y);
+                  g2d.drawString("                           "+totals+"",10,y);y+=yShift;
+                  }
+                  int bayars = Integer.parseInt(txt_bayar.getText());
+                  double angka3 = (double) bayars;
+                  String bayar = String.format("%,.0f", angka3).replaceAll(",", ".");
+                  String grand_total = txt_total.getText();
+                  String kembalian = txt_kembalian.getText();
+                  g2d.drawString("-------------------------------------",10,y);y+=yShift; 
+                  g2d.drawString("               Bayar",10,y);
+                  g2d.drawString("                           "+bayar+"",10,y);y+=yShift;
+                  g2d.drawString("               Total",10,y);
+                  g2d.drawString("                           "+grand_total+"",10,y);y+=yShift;
+                  g2d.drawString("               Kembalian",10,y);
+                  g2d.drawString("                           "+kembalian+"",10,y);y+=yShift;
+                  g2d.drawString("-------------------------------------",10,y);y+=yShift;
+                  g2d.drawString("          Free Home Delivery         ",10,y);y+=yShift;
+                  g2d.drawString("             03111111111             ",10,y);y+=yShift;
+                  g2d.drawString("*************************************",10,y);y+=yShift;
+                  g2d.drawString("    THANKS TO VISIT OUR RESTUARANT   ",10,y);y+=yShift;
+                  g2d.drawString("*************************************",10,y);y+=yShift;
+                   }
+                   catch(Exception r){
+                   r.printStackTrace();
+                   }
+
+                   result = PAGE_EXISTS;    
+                }    
+                    return result;    
+        }
+   }
+    
     private void btn_simpanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_simpanActionPerformed
         // TODO add your handling code here:
         if (txt_bayar.getText().equals("")) {
             JOptionPane.showMessageDialog(null, "Masukan pembayaran", "Kesalahan", JOptionPane.ERROR_MESSAGE);
         } else {
+            PrinterJob pj = PrinterJob.getPrinterJob();        
+        pj.setPrintable(new BillPrintable(),getPageFormat(pj));
+        try {
+             pj.print();
+          
+        }
+         catch (PrinterException ex) {
+                 ex.printStackTrace();
+        }
             try {
                 Date skrg = new Date();
                 SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
